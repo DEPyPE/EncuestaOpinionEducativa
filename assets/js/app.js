33fa816c748b4ed32c0f1ce29994d2053f2ce5b0
DEPYPE <wa_moreno@seg.guanajuato.gob.mx>
2022-03-15 13:53:56 -0600

    Posicion del tooltip

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,5 +7,11 @@ $(function(){
 var mediaqueryList = window.matchMedia("(max-width: 751px)");
 
 mediaqueryList.addListener( function(EventoMediaQueryList) {
-      $('#autoridades').attr('data-position', 'top');
+      $('#autoridades').attr('data-position', 'bottom');
+});
+
+var mediaqueryList2 = window.matchMedia("(min-width: 752px)");
+
+mediaqueryList2.addListener( function(EventoMediaQueryList) {
+    $('#autoridades').attr('data-position', 'right');
 });
