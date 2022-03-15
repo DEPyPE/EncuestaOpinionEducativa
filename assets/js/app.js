f6dfba2220e9dd2526ca021173dedf42cf073377
DEPYPE <wa_moreno@seg.guanajuato.gob.mx>
2022-03-15 13:48:22 -0600

    Cambiamos la posición del tooltip de acuerdo a las dimensiones de la pantalla

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,9 @@ $(function(){
     $('.tooltipped').tooltip();
     console.log( "Ready!" );
 });
+
+var mediaqueryList = window.matchMedia("(max-width: 751px)");
+
+mediaqueryList.addListener( function(EventoMediaQueryList) {
+      $('#autoridades').attr('data-position', 'top');
+});
